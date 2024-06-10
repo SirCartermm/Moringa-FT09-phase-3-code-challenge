8a907c50c8508964af056fc62edd3ca5c39bfb60
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-10 22:29:46 +0300

    modified magazine.py

--- models/author.py
+++ models/author.py
@@ -21,4 +21,17 @@ class Author:
     def name(self, value):
         if not isinstance(value, str) or len(value) < 1:
             raise ValueError("Name must be a non-empty string")
-        self._name = value
+        self._name = value 
+
+
+
+class Author:
+    # ...
+
+    def articles(self):
+        # Use SQLAlchemy's ORM to retrieve articles associated with the author
+        return db.session.query(Article).join(Author).filter(Article.author_id == self.id).all()
+
+    def magazines(self):
+        # Use SQLAlchemy's ORM to retrieve magazines associated with the author
+        return db.session.query(Magazine).join(Article).join(Author).filter(Article.author_id == self.id).all()
