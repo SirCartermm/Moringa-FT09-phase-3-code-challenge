88d6e83ce2ca3fa28f649594b488c0d1c917afc5
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-10 22:11:22 +0300

    modified setup.py

--- database/setup.py
+++ database/setup.py
@@ -8,3 +8,20 @@ class Author(Base):
     __tablename__ = 'authors'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+
+class Magazine(Base):
+    __tablename__ = 'magazines'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+
+class Article(Base):
+    __tablename__ = 'articles'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    magazine_id = Column(Integer, ForeignKey('magazines.id'))
+    author = relationship('Author', backref='articles')
+    magazine = relationship('Magazine', backref='articles')
+
+engine = create_engine('sqlite:///database.db')
