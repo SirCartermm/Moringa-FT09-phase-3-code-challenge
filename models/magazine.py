50ff0ae0b11b4349f9f52229e6ec68ce4a6747f3
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-10 22:27:58 +0300

    modified magazine.py

--- models/magazine.py
+++ models/magazine.py
@@ -1,3 +1,9 @@
+import dbm
+from sqlite3 import dbapi2
+
+from database.setup import Article, Author
+
+
 class Magazine:
     def __init__(self, id, name, category):
         self.id = id
@@ -32,4 +38,31 @@ class Magazine:
     def category(self, value):
         if not isinstance(value, str) or len(value) < 1:
             raise ValueError("Category must be a non-empty string")
-        self._category = value
+        self._category = value 
+
+
+class Magazine:
+    # ...
+
+    def articles(self):
+        # Use SQLAlchemy's ORM to retrieve articles associated with the magazine
+        return dbm.session.query(Article).join(Magazine).filter(Article.magazine_id == self.id).all()
+
+    def contributors(self):
+        # Use SQLAlchemy's ORM to retrieve authors associated with the magazine
+        return dbapi2.session.query(Author).join(Article).join(Magazine).filter(Article.magazine_id == self.id).all()
+
+    def article_titles(self):
+        # Use SQLAlchemy's ORM to retrieve article titles associated with the magazine
+        return [article.title for article in self.articles()]
+
+    def contributing_authors(self):
+        # Use SQLAlchemy's ORM to retrieve authors who have written more than 2 articles for the magazine
+        authors = {}
+        for article in self.articles():
+            author = article.author
+            if author in authors:
+                authors[author] += 1
+            else:
+                authors[author] = 1
+        return [author for author, count in authors.items() if count > 2]
