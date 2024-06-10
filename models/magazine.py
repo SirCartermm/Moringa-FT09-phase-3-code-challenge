9d96a2604cbdaae9f2235df9f799ef1c7e0d358e
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-10 22:24:02 +0300

    modified magazine.py

---
+++ models/magazine.py
@@ -0,0 +1,15 @@
+class Magazine:
+    def __init__(self, id, name, category):
+        self.id = id
+        self.name = name
+        self.category = category
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Id must be an integer")
+        self._id = value
