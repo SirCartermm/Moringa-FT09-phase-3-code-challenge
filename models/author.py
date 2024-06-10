d46b48387164e1c4b9f99f3b19c7ce43247cd035
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-10 22:15:12 +0300

    modified author.py

--- models/author.py
+++ models/author.py
@@ -9,3 +9,16 @@ class Author:
 
     @id.setter
     def id(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Id must be an integer")
+        self._id = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) < 1:
+            raise ValueError("Name must be a non-empty string")
+        self._name = value
