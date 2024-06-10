329ea2c9aa35c1fd9ccd4ddc9dd02901a3258963
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-10 22:08:41 +0300

    modified articles.py

--- models/article.py
+++ models/article.py
@@ -9,3 +9,13 @@ class Article:
         return self._title
 
     @title.setter
+    def title(self, value):
+        if not isinstance(value, str) or len(value) < 5 or len(value) > 50:
+            raise ValueError("Title must be a string between 5 and 50 characters")
+        self._title = value
+
+    def author(self):
+        return self.author
+
+    def magazine(self):
+        return self.magazine
