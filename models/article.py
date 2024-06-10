d02e35aac7c3ce309f03895245bde48a452657a5
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-10 22:08:06 +0300

    modified articles.py

---
+++ models/article.py
@@ -0,0 +1,11 @@
+class Article:
+    def __init__(self, author, magazine, title):
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
